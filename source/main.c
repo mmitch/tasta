ebdbf3b2007dac4f2357c37e1fea61dcc2f52dd7
Christian Garbs <mitch@cgarbs.de>
2015-07-16 23:49:22 +0200

    reformat code

--- source/main.c
+++ source/main.c
@@ -53,7 +53,7 @@ PB5     reset (in hardware; unused here)
 static void hardwareInit(void)
 {
 	uchar i;
-	uchar   calibrationValue;
+	uchar calibrationValue;
 
 	calibrationValue = eeprom_read_byte(0); /* calibration value from last time */
 	if (calibrationValue != 0xff)
@@ -224,7 +224,7 @@ static void buildReport(uchar key)
 
 uchar usbFunctionSetup(uchar data[8])
 {
-	usbRequest_t    *rq = (void *)data;
+	usbRequest_t *rq = (void *)data;
 
 	usbMsgPtr = reportBuffer;
 	if ((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS) /* class request type */
@@ -263,32 +263,39 @@ uchar usbFunctionSetup(uchar data[8])
  */
 static void calibrateOscillator(void)
 {
-    int frameLength, targetLength = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
-    int bestDeviation = 9999;
-    uchar trialCal, bestCal, step, region;
+	int frameLength, targetLength = (unsigned)(1499 * (double)F_CPU / 10.5e6 + 0.5);
+	int bestDeviation = 9999;
+	uchar trialCal, bestCal, step, region;
 
-    // do a binary search in regions 0-127 and 128-255 to get optimum OSCCAL
-    for(region = 0; region <= 1; region++) {
-        frameLength = 0;
-        trialCal = (region == 0) ? 0 : 128;
+	/* do a binary search in regions 0-127 and 128-255 to get optimum OSCCAL */
+	for (region = 0; region <= 1; region++)
+	{
+		frameLength = 0;
+		trialCal = (region == 0) ? 0 : 128;
         
-        for(step = 64; step > 0; step >>= 1) { 
-            if(frameLength < targetLength) // true for initial iteration
-                trialCal += step; // frequency too low
-            else
-                trialCal -= step; // frequency too high
+		for (step = 64; step > 0; step >>= 1)
+		{
+			if (frameLength < targetLength) /* true for initial iteration */
+			{
+				trialCal += step; /* frequency too low */
+			}
+			else
+			{
+				trialCal -= step; /* frequency too high */
+			}
                 
-            OSCCAL = trialCal;
-            frameLength = usbMeasureFrameLength();
-            
-            if(abs(frameLength-targetLength) < bestDeviation) {
-                bestCal = trialCal; // new optimum found
-                bestDeviation = abs(frameLength -targetLength);
-            }
-        }
-    }
+			OSCCAL = trialCal;
+			frameLength = usbMeasureFrameLength();
+			
+			if (abs(frameLength-targetLength) < bestDeviation)
+			{
+				bestCal = trialCal; /* new optimum found */
+				bestDeviation = abs(frameLength -targetLength);
+			}
+		}
+	}
 
-    OSCCAL = bestCal;
+	OSCCAL = bestCal;
 }
 
 void usbEventResetReady(void)
